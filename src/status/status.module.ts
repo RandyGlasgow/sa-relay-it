883f4cda3d87eef98da40f7cbdc703e4d537ab9e
randyglasgow <glasgowrandy@gmail.com>
2022-04-27 22:40:37 -0400

    Removes initial load

--- src/status/status.module.ts
+++ src/status/status.module.ts
@@ -1,40 +1,13 @@
-import { Module, NotFoundException } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StatusController } from './status.controller';
 import { StatusService } from './status.service';
 import { Status } from './status.entity';
-import { CreateStatusDto } from './dto/create-status.dto';
-import { Repository } from 'typeorm/repository/Repository';
 
 @Module({
   controllers: [StatusController],
   providers: [StatusService],
   imports: [TypeOrmModule.forFeature([Status])],
 })
-export class StatusModule {
-  constructor(private readonly statusService: StatusService) {}
-  async onModuleInit() {
-    // read in the default status codes from the file
-    const codes = require('../../default_status_codes.json');
-
-    // checks
-    if (!codes || codes.length === 0) return;
-
-    // create the status codes
-    codes.forEach((code: CreateStatusDto) => {
-      const { name, message, statusCode, isActive } = code;
-
-      // check if the status code already exists
-      const existing = this.statusService.findOne(statusCode).catch((err) => {
-        // create the status code
-        this.statusService.create({
-          name,
-          message,
-          statusCode,
-          isActive,
-        });
-      });
-    });
-  }
-}
+export class StatusModule {}
