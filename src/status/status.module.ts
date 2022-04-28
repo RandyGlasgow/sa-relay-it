5e11453544ceca896ff38758b0d3c4b3bfa837c1
randyglasgow <glasgowrandy@gmail.com>
2022-04-27 22:49:13 -0400

    Adds initial statuscodes on build

--- src/status/status.module.ts
+++ src/status/status.module.ts
@@ -1,13 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Module, NotFoundException } from '@nestjs/common';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
 import { StatusController } from './status.controller';
 import { StatusService } from './status.service';
 import { Status } from './status.entity';
+import { CreateStatusDto } from './dto/create-status.dto';
+import { Repository } from 'typeorm/repository/Repository';
 
 @Module({
   controllers: [StatusController],
   providers: [StatusService],
   imports: [TypeOrmModule.forFeature([Status])],
 })
-export class StatusModule {}
+export class StatusModule {
+  constructor(private readonly statusService: StatusService) {}
+  async onModuleInit() {
+    // read in the default status codes from the file
+    const codes = require('../../default_status_codes.json');
+
+    // checks
+    if (!codes || codes.length === 0) return;
+
+    // create the status codes
+    codes.forEach((code: CreateStatusDto) => {
+      const { name, message, statusCode, isActive } = code;
+
+      // check if the status code already exists
+      this.statusService.findOne(statusCode).catch(() => {
+        // create the status code
+        this.statusService.create({
+          name,
+          message,
+          statusCode,
+          isActive,
+        });
+      });
+    });
+  }
+}
